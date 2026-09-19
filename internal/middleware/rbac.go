339e25b98fa92001a493686479ad1813d21fb7ed
Ritik-x <133737391+Ritik-x@users.noreply.github.com>
2026-09-19 17:41:39 +0530

    middleware

---
+++ internal/middleware/rbac.go
@@ -0,0 +1,45 @@
+package middleware
+
+import (
+	"net/http"
+	"team-access-control/internal/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RequiredPermission(rbacservice *services.RBACService , requiredPermission string ) gin.HandlerFunc{
+	return func(c *gin.Context){
+		userID := c.GetString("user_id")
+		organizationID := c.GetString("organization_id")
+		if userID == "" || organizationID == " "{
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "authentication context missing",
+			})
+			c.Abort()
+			return
+		}
+
+		hasPermission ,err := rbacservice.HasPermission(
+			c.Request.Context(),
+				userID,
+			organizationID,
+			requiredPermission,
+		)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "failed to check permission",
+			})
+			c.Abort()
+			return
+		}
+		if !hasPermission {
+			c.JSON(http.StatusForbidden, gin.H{
+				"error": "permission denied",
+			})
+			c.Abort()
+			return
+		}
+			c.Next()
+	}
+}
